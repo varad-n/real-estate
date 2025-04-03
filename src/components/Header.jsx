55adec59f6a92a8768884e310aba64a5313fb9ce
varad-n <hvi924926@gmail.com>
2025-04-03 23:56:35 +0530

    project in progress

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden'></div>
+  )
+}
+
+export default Header
