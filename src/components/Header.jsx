c56053870e8e18e14e9f08cdd17cddcc28ba3515
varad-n <hvi924926@gmail.com>
2025-04-04 23:40:16 +0530

    project in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,13 @@ const Header = () => {
     <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' 
     style={{backgroundImage: "url('../public/header_img.png')"}} id='Header'>
       <Navbar />
-      <div>
-        <h2>Explore Homes that fit your dreams</h2>
-        <a href=''>Projects</a>
-        <a href=''>Contact Us</a>
+      <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
+        <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block 
+        max-w-3xl font-semibold pt-20'>Explore Homes that fit your dreams</h2>
+        <div className='space-x-6 mt-16'>
+          <a href='' className='border border-white px-8 py-3 rounded'>Projects</a>
+          <a href='' className='bg-blue-500 px-8 py-3 rounded'>Contact Us</a>
+        </div>
       </div>
     </div>
   )
