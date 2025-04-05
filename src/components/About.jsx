c98ed9f4a7d6dfc747e317548605f6e67f110d6e
varad-n <hvi924926@gmail.com>
2025-04-05 18:31:28 +0530

    project in progress

---
+++ src/components/About.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center contianer mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden'
+    id='About'>
+       <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span 
+            className='underline underline-offset-4 decoration-1 under font-light'>Our Brand</span></h1>
+       <p className='text-gray-500 max-w-80 text-center mb-8'>Passionate about Properties, Dedicated to Your Vision</p>
+       <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+            <img src={assets.brand_img} alt='' className='w-full sm:w-1/2 max-w-lg'/>
+            <div className='flex flex-col items-center md:items-start mt-10 text-gray-600'>
+                <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>10+</p>
+                        <p>Years of Excellence</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>12+</p>
+                        <p>Projects Completed</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>20+</p>
+                        <p>Mn. Sq. Ft. Delivered</p>
+                    </div>
+                    <div>
+                        <p className='text-4xl font-medium text-gray-800'>25+</p>
+                        <p>Ongoing Projects</p>
+                    </div>
+                </div>
+                <p className='my-10 max-w-0-lg'>Real estate is considered real property that includes land and anything permanently
+                    attached to it or built on it, whether natural or man-made. Investing in real estate
+                    includes purchasing a home, rental property, or land. Indirect investment in real
+                    estate can be made via REITs or through pooled real estate investment.
+                </p>
+            </div>
+       </div>
+    </div>
+  )
+}
+
+export default About
