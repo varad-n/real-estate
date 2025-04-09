18fec8b17b1a7c3ac60ef0b872fc0de1f7629459
varad-n <hvi924926@gmail.com>
2025-04-09 12:57:18 +0530

    project in progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
