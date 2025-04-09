e73907c962e87863f867c5ae981b25195d9ffa33
varad-n <hvi924926@gmail.com>
2025-04-09 18:11:18 +0530

    project completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,16 +3,35 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+        <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
+            <div className='w-full md:w-1/3 mb-8 md:mb-0'>
                 <img src={assets.logo_dark} alt='' />
-                <p>Dummy Para: Real estate encompasses land and anything permanently attached to it, like buildings and fixtures. It's a form of real property, distinct from personal property that isn't permanently attached, such as vehicles or fur</p>
+                <p className='text-gray-400 mt-4'>Dummy Para: Real estate encompasses land and anything permanently attached to it, like buildings and fixtures. It's a form of real property.</p>
             </div>
-            <div></div>
-            <div></div>
+            <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+              <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+              <ul className='flex flex-col gap-2 text-gray-400'>
+                <a href='#Header' className='hover:text-white'>Home</a>
+                <a href='#About' className='hover:text-white'>About Us</a>
+                <a href='#Contact' className='hover:text-white'>Contact Us</a>
+                <a href='#' className='hover:text-white'>Privacy Policy</a>
+                <br />
+              </ul>
+            </div>
+            <div className='w-full md:w-1/3'>
+              <h3 className='text-white text-lg font-bold mb-4'>Subscribe to our Newsletter</h3>
+              <p className='text-gray-400 mb-4 max-w-80'>The latest new, articles, and resources, sent to your inbox weekly.</p>
+              <div className='flex gap-2'>
+                <input type='email' placeholder='Enter your Email'
+                className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto' />
+                <button className='py-2 px-4 rounded bg-blue-500 text-white'>Subscribe</button>
+              </div>
+            </div>
         </div>
-        <div></div>
+        <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+          Copyright 2025 © varad. All Rights Reserved.
+        </div>
     </div>
   )
 }
