f5829d1c7fa510458ff4665295850a6e70ce4156
varad-n <hvi924926@gmail.com>
2025-04-09 13:43:33 +0530

    project in progress

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from './components/Projects'
 import Testimonials from './components/Testimonials'
 import Contact from './components/Contact'
 import { ToastContainer, toast } from 'react-toastify';
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Projects />
       <Testimonials />
       <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <div>
+                <img src={assets.logo_dark} alt='' />
+                <p>Dummy Para: Real estate encompasses land and anything permanently attached to it, like buildings and fixtures. It's a form of real property, distinct from personal property that isn't permanently attached, such as vehicles or fur</p>
+            </div>
+            <div></div>
+            <div></div>
+        </div>
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
